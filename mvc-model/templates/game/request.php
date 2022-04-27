fa9ba3a3eb6b1b128d0118eb346ab7bdcb12ecb6
Amelie Zeller <aze129646@stud.gibb.ch>
2022-04-27 14:11:19 +0200

    Icon hinzugefügt

--- mvc-model/templates/game/request.php
+++ mvc-model/templates/game/request.php
@@ -15,7 +15,7 @@
 
     <div class="form-field">
         <input class="form-input" type="text" id="email" name="email" placeholder="Email" required>
-        <i class='bx bxs-user form-icon'></i>
+        <i class='bx bx-at form-icon'></i>
     </div>
 
     <div class="form-field">
