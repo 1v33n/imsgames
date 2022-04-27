5143ddf162e028dce2260b228363b4e801eef6cc
Amelie Zeller <aze129646@stud.gibb.ch>
2022-04-27 08:42:53 +0200

    Footer angepasst

--- mvc-model/templates/footer.php
+++ mvc-model/templates/footer.php
@@ -2,7 +2,7 @@
 
     <footer class="footer">
       <div class="container">
-        <span class="text-muted">&copy; ICT Berufsbildungcenter AG 2016</span>
+        <span class="text-muted">&copy; imsgames 2022</span>
       </div>
     </footer>
 
